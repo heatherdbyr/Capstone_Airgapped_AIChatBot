20235dcca9fa6a9acc80b52c3f6e86f7aa5c04fc
Charlie Connor <charlieconnor03@gmail.com>
2023-10-12 17:49:43 +1100

    date stuff, change temps

--- Sample_demo/demo.py
+++ Sample_demo/demo.py
@@ -12,8 +12,6 @@ API_URL = "https://ln9afaz9wxel05le.us-east-1.aws.endpoints.huggingface.cloud/"
 
 MODEL_NAME = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
 
-CURR_DOC = []
-
 headers = {
 	"Authorization": "Bearer TeWAqUDMIdoqDyPtnisCRPddnmdPBhScjqWuIWkQtvyOuPIqBeRtpycLbXBrKFldVGLrTKEjVGIkbHzjBkmqYMoHjjWTgoBzhCBFaeSIACuAdHxvGLBiXgYJVzidmbKY",
 	"Content-Type": "application/json"
@@ -27,7 +25,6 @@ def display_chat_history(messages):
 
 
 def elasticSearch(userInput):
-    global CURR_DOC
     ELASTIC_PASSWORD = "xel8PSGj3Bofb8vuR0FQK2NS"
 
     CLOUD_ID = "Test_Deployment:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ0MzJmODIyY2RmZDI0MWU1ODY5OTFiMGJiZGI1YjgzNyRhNjE0MzU1M2FhM2U0M2NhYjIwOTAyOTdhNWM2N2EyMA=="
@@ -37,9 +34,13 @@ def elasticSearch(userInput):
         basic_auth=("elastic", ELASTIC_PASSWORD)
     )
 
+    # Attempt to parse the user input as a date
     try:
         parsed_date = parse(userInput, fuzzy=True)
+        print(parsed_date)
+        # Check if the parsed date has a year, month, and day
         if parsed_date.year is None or parsed_date.month is None or parsed_date.day is None:
+            print("Invalid date")
             raise ValueError("Invalid date")
         # If a date was successfully parsed, create a date-based query
         date_query = {
@@ -51,6 +52,7 @@ def elasticSearch(userInput):
         # If parsing as a date fails, treat it as a regular text query
         date_query = None
 
+    # Create a text query to search multiple fields for the user input
     text_query = {
         "multi_match": {
             "query": userInput,
@@ -62,19 +64,25 @@ def elasticSearch(userInput):
     # Create a bool query to combine the date and text queries
     query = {
         "bool": {
-            "should": []
+            "must": []
         }
     }
 
+    # Add the date query to the bool query if it exists
     if date_query:
-        query["bool"]["should"].append(date_query)
-        print('date')
+        query["bool"]["must"].append(date_query)
 
-    query["bool"]["should"].append(text_query)
+    # Add the text query to the bool query
+    query["bool"]["must"].append(text_query)
 
+    # Create a search object using the Elasticsearch client and the index name
     s = Search(using=es, index="search-llama2")
+
+    # Set the query for the search object using the bool query
     s = s.query(query)
-    responses = s.execute()
+
+    # Execute the search and return the results
+    responses = s.execute()    
 
     response_list = []
     response_counter = 0
@@ -89,40 +97,38 @@ def elasticSearch(userInput):
             response['author'] = hit.author
         elif hasattr(hit, 'authors'):
             response['authors'] = hit.authors
+        
+        if 'nytimes.com' in response['link']:
+            clean_link = response['link'].split('http')[-1]
+            response['link'] = 'http' + clean_link
 
         response_list.append(response)
 
-        # print("Relevant Article Headline:", hit.headline)
-        # print("Relevant Article Date:", hit.date)
-        # print("Relevant Article Link:", hit.link)
-        # print("Relevant Article Short Description:", hit.short_description)
-        # print("\n")
-
     # Check if any results were returned
     if len(response_list) > 0:
-        CURR_DOC = response_list
-        return CURR_DOC
+        return response_list
     else:
         return None
     
 def isFollowUp(userInput, history): 
 
     followUpTemplate = f"""
-        [SYSTEM] Answer Question with only a one word answer either TRUE or FALSE. If you don't know answer with FALSE.
-        [SYSTEM] This history of conversation: '{history}'
+        [SYSTEM] Answer question with only a one word answer either TRUE or FALSE.
+        [SYSTEM] This is the last thing that the assistant (you) answered with: '{history[-2]}'
         [SYSTEM] User input: '{userInput}'
-        [SYSTEM] Is this user input a follow up question of the user's last question or assistant's response? 
+        Is this userInput asking about the assistant's (your) last response? Answer TRUE or FALSE, If you don't know with 100% certainty, answer with FALSE. 
     """
+    print(history[-2])
     output = replicate.run(
         MODEL_NAME,
         input={"prompt": followUpTemplate,
-        "temperature":0.75,
+        "temperature":0.10,
            "max_new_tokens":8000,
            "max_length":8000
         }
     )
     response = ''.join(output)
-    print(response)
+    print(f"Follow up response: {response}")
 
     if "TRUE" in response:
         return True
@@ -130,31 +136,32 @@ def isFollowUp(userInput, history):
         return False
     
 
-def chatCompletion(userInput, history): 
+def chatCompletion(userInput, history):
     #use case document
     searchErr = "Sorry I couldn't find any articles related to your query. Please try again."
     response = ""
 
   #  Check if the conversation has history
+    response_list = None
     if len(history) > 2:
         #Check if the user input is a follow up question
         if isFollowUp(userInput, history):
-            #if so make use case document the current document
-            response_list = CURR_DOC
-            print(response_list)
+            print('FOLLOW')
+            response_list = st.session_state.prev_response_list
         else:
-            #if not make a new search
             response_list = elasticSearch(userInput)
-
+            st.session_state.prev_response_list = response_list
+        #if not make a new search
     else:
         response_list = elasticSearch(userInput)
-        print(response_list)
-        print(CURR_DOC)
+        st.session_state.prev_response_list = response_list
+
 
     if response_list is None:
         return searchErr
 
     # Create a list of article titles and links for a more conversational response
+    print(response_list)
     article_responses = []
     for idx, article in enumerate(response_list, start=1):
         if 'author' in article:
@@ -169,24 +176,28 @@ def chatCompletion(userInput, history):
 
     # Join the article responses with line breaks
     article_list = "\n".join(article_responses)
+    print(userInput)
 
     template = f"""
     [SYSTEM] You are a search assistant with access to the HuffPost archives from 2012 to 2022.
     [SYSTEM] This is the history of our conversation so far: '{history}'
     [SYSTEM] Latest User Input: '{userInput}'
 
-    [SYSTEM] Here are some relevant articles related to the latest input:
+    [SYSTEM] Here are some relevant articles related to could possibly be related to the latest input:
     {article_list}
 
-    [SYSTEM] Check the above article_list, and if they are relevant to the userInput, then provide the info to the user, ENSURE you give the links and maybe give a brief description of each article. 
+    [SYSTEM] Answer the userInput to the best of your ability.
+    [SYSTEM] If the userInput is just trying to have a conversation, and is unrelated to the article_list, then just have a conversation. DO NOT provide any articles if a question is not asked.
+    [SYSTEM] Check the above article_list, and if any article is relevant to the userInput, then provide it to the user, **ENSURE** you give the related LINK.
+    [SYSTEM] DO NOT EDIT ANY OF THE ARTICLE INFORMATION!!!!! You can add to it, but do not remove or change anything.
+    [SYSTEM] If the userInput refers to something previously mentioned, ensure you check both the userInput and the history to give the best answer possible. ***NEVER EVER REPEAT YOURSELF!!!!
     [SYSTEM] If you don't know the answer to a question, DO NOT make anything up, just say you don't know.
-    [SYSTEM] Please start a new line for each new article.
     """
 
     output = replicate.run(
         MODEL_NAME,
         input={"prompt": template,
-            "temperature": 0.75,
+            "temperature": 1.00,
             "max_new_tokens": 8000,
             "max_length": 8000
             }
@@ -200,8 +211,8 @@ def chatCompletion(userInput, history):
 
 
 def main(): 
-
     st.title('Llama 2 Air-Gapped ChatBot')
+
 
     # Initialize chat history
     if "messages" not in st.session_state:
