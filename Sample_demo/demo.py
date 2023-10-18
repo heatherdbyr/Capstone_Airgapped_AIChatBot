017321729fc0c384e846ae3365c979cbc9ca5598
Charlie Connor <charlieconnor03@gmail.com>
2023-10-18 14:08:45 +1100

    final edits

--- Sample_demo/demo.py
+++ Sample_demo/demo.py
@@ -5,18 +5,42 @@ import replicate
 import streamlit as st 
 import time
 import os
+import re
 
 # Set the environment variable
 os.environ['REPLICATE_API_TOKEN'] = 'r8_X1qWO35dTBKsIN7GQbSrbuWVBu89Onf1c8SGt'
 API_URL = "https://ln9afaz9wxel05le.us-east-1.aws.endpoints.huggingface.cloud/"
 
-MODEL_NAME = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
+MODEL_NAME = "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3"
 
 headers = {
 	"Authorization": "Bearer TeWAqUDMIdoqDyPtnisCRPddnmdPBhScjqWuIWkQtvyOuPIqBeRtpycLbXBrKFldVGLrTKEjVGIkbHzjBkmqYMoHjjWTgoBzhCBFaeSIACuAdHxvGLBiXgYJVzidmbKY",
 	"Content-Type": "application/json"
 }
 
+#words can be filtered out of searches to elastic
+MISC_WORDS = [
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+    "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
+    "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+    "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have",
+    "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's",
+    "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
+    "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself",
+    "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of",
+    "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves",
+    "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's",
+    "should", "shouldn't", "so", "some", "such", "tell", "than", "that", "that's",
+    "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those",
+    "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we",
+    "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+    "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why",
+    "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're",
+    "you've", "your", "yours", "yourself", "yourselves", "hi", "hello", "love", "can",
+    "about"
+]
 
 def display_chat_history(messages):
     for message in messages:
@@ -34,97 +58,116 @@ def elasticSearch(userInput):
         basic_auth=("elastic", ELASTIC_PASSWORD)
     )
 
-    # Attempt to parse the user input as a date
-    try:
-        parsed_date = parse(userInput, fuzzy=True)
-        print(parsed_date)
-        # Check if the parsed date has a year, month, and day
-        if parsed_date.year is None or parsed_date.month is None or parsed_date.day is None:
-            print("Invalid date")
-            raise ValueError("Invalid date")
-        # If a date was successfully parsed, create a date-based query
-        date_query = {
-            "term": {
-                "date": parsed_date.strftime("%Y-%m-%d")
-            }
-        }
-    except ValueError:
-        # If parsing as a date fails, treat it as a regular text query
-        date_query = None
+    keywords = [word for word in userInput.split() if word.lower() not in MISC_WORDS]
+    subject_query = " ".join(keywords)
 
-    # Create a text query to search multiple fields for the user input
+    # Attempt to parse the user input as a date
+    date_query = None
+    try:
+        if re.search(r'\d.*\d', userInput):
+            parsed_date = parse(userInput, fuzzy=True)
+            
+            # Check if the parsed date has a year, month, and day
+            if parsed_date.year and parsed_date.month and parsed_date.day:
+                date_query = {
+                    "term": {
+                        "date": parsed_date.strftime("%Y-%m-%d")
+                    }
+                }
+    except ValueError:
+        # If parsing as a date fails, simply pass
+        pass
+
+    # Create a text query
     text_query = {
         "multi_match": {
-            "query": userInput,
-            "fields": ["authors", "headline", "category", "short_description", "link"],
-            "type": "best_fields"
+            "query": subject_query,
+            "fields": [
+                "headline^3",  # boost headlines
+                "short_description^2",  # boost short descriptions
+                "authors",
+                "category",
+                "link"
+            ],
+            "type": "best_fields",
+            "fuzziness": "AUTO"
         }
     }
 
-    # Create a bool query to combine the date and text queries
+    # Modify the bool query to use "should" for boosting relevance
     query = {
         "bool": {
-            "must": []
+            "should": [
+                text_query,
+                {
+                    "match": {
+                        "headline": subject_query
+                    }
+                },
+                {
+                    "match": {
+                        "short_description": subject_query
+                    }
+                }
+            ]
         }
     }
 
-    # Add the date query to the bool query if it exists
+    # Add the date query if it exists
     if date_query:
+        if "must" not in query["bool"]:
+            query["bool"]["must"] = []
         query["bool"]["must"].append(date_query)
 
-    # Add the text query to the bool query
-    query["bool"]["must"].append(text_query)
+    # Create a search object
+    s = Search(using=es, index="search-llama2").query(query)
 
-    # Create a search object using the Elasticsearch client and the index name
-    s = Search(using=es, index="search-llama2")
-
-    # Set the query for the search object using the bool query
-    s = s.query(query)
-
-    # Execute the search and return the results
-    responses = s.execute()    
+    # Execute the search
+    responses = s.execute()
 
     response_list = []
-    response_counter = 0
-    for hit in responses:
-        if response_counter >= 10:
-            break
-        response_counter += 1
-
-        response = {"headline": hit.headline, "date": hit.date, "link": hit.link, "short_description": hit.short_description, "category": hit.category}
+    for hit in responses[:10]:
+        response = {
+            "headline": hit.headline,
+            "date": hit.date,
+            "link": hit.link,
+            "short_description": hit.short_description,
+            "category": hit.category
+        }
 
         if hasattr(hit, 'author'):
             response['author'] = hit.author
         elif hasattr(hit, 'authors'):
             response['authors'] = hit.authors
         
-        if 'nytimes.com' in response['link']:
+        if response['link'].count('http') > 1:
             clean_link = response['link'].split('http')[-1]
             response['link'] = 'http' + clean_link
 
-        response_list.append(response)
+        response_list.append(response)  
 
-    # Check if any results were returned
-    if len(response_list) > 0:
-        return response_list
-    else:
-        return None
+    return response_list if response_list else None
     
+
 def isFollowUp(userInput, history): 
 
-    followUpTemplate = f"""
-        [SYSTEM] Answer question with only a one word answer either TRUE or FALSE.
-        [SYSTEM] This is the last thing that the assistant (you) answered with: '{history[-2]}'
-        [SYSTEM] User input: '{userInput}'
-        Is this userInput asking about the assistant's (your) last response? Answer TRUE or FALSE, If you don't know with 100% certainty, answer with FALSE. 
+    followUpSys = f"""
+        [SYSTEM] Context is key. Please consider:
+        [SYSTEM] Previous user input: '{history[-3]}'
+        [SYSTEM] Assistant's last response: '{history[-2]}'
+        [SYSTEM] Current user input: '{userInput}'
     """
-    print(history[-2])
+    followUpTemplate = f"""
+    Answer with TRUE or FALSE only. Is the current userInput, a follow up question on the same exact topic as the previous user input?
+    """
+
     output = replicate.run(
         MODEL_NAME,
-        input={"prompt": followUpTemplate,
+        input={"system_prompt": followUpSys,
+            "prompt": followUpTemplate,
         "temperature":0.10,
-           "max_new_tokens":8000,
-           "max_length":8000
+           "max_new_tokens":128,
+           "max_length":128
         }
     )
     response = ''.join(output)
@@ -146,7 +189,6 @@ def chatCompletion(userInput, history):
     if len(history) > 2:
         #Check if the user input is a follow up question
         if isFollowUp(userInput, history):
-            print('FOLLOW')
             response_list = st.session_state.prev_response_list
         else:
             response_list = elasticSearch(userInput)
@@ -161,7 +203,6 @@ def chatCompletion(userInput, history):
         return searchErr
 
     # Create a list of article titles and links for a more conversational response
-    print(response_list)
     article_responses = []
     for idx, article in enumerate(response_list, start=1):
         if 'author' in article:
@@ -171,37 +212,41 @@ def chatCompletion(userInput, history):
         else:
             author_str = ""
 
-        article_response = f"{idx}. [{article['headline']}]({article['link']}){author_str} - {article['short_description']} - {article['date']}"
+        article_response = f"{idx}. [{article['headline']}](Link: {article['link']}) - Author: {author_str} - {article['short_description']} - {article['date']}"
         article_responses.append(article_response)
 
     # Join the article responses with line breaks
     article_list = "\n".join(article_responses)
-    print(userInput)
+    print(article_list)
+
+
+    system_prompt = """
+        [SYSTEM] If the user initiates a conversation, engage without sending articles. You should be friendly in all responses.
+        [SYSTEM] If the user asks a question, respond concisely with relevant article details.
+        [SYSTEM] If asked for specific details (date, author, link), provide them.
+        [SYSTEM] Avoid echoing the user's input.
+        [SYSTEM] Only provide genuine articles; no fakes.
+    """
+
 
     template = f"""
-    [SYSTEM] You are a search assistant with access to the HuffPost archives from 2012 to 2022.
-    [SYSTEM] This is the history of our conversation so far: '{history}'
+    [SYSTEM] Conversation history: '{history}'
     [SYSTEM] Latest User Input: '{userInput}'
-
-    [SYSTEM] Here are some relevant articles related to could possibly be related to the latest input:
-    {article_list}
-
-    [SYSTEM] Answer the userInput to the best of your ability.
-    [SYSTEM] If the userInput is just trying to have a conversation, and is unrelated to the article_list, then just have a conversation. DO NOT provide any articles if a question is not asked.
-    [SYSTEM] Check the above article_list, and if any article is relevant to the userInput, then provide it to the user, **ENSURE** you give the related LINK.
-    [SYSTEM] DO NOT EDIT ANY OF THE ARTICLE INFORMATION!!!!! You can add to it, but do not remove or change anything.
-    [SYSTEM] If the userInput refers to something previously mentioned, ensure you check both the userInput and the history to give the best answer possible. ***NEVER EVER REPEAT YOURSELF!!!!
-    [SYSTEM] If you don't know the answer to a question, DO NOT make anything up, just say you don't know.
+    [SYSTEM] Articles found:
+    {article_list} 
     """
 
     output = replicate.run(
         MODEL_NAME,
-        input={"prompt": template,
-            "temperature": 1.00,
-            "max_new_tokens": 8000,
-            "max_length": 8000
-            }
+        input={"system_prompt": system_prompt,
+            "prompt": template,
+               "temperature": 0.20,
+               "max_new_tokens": 8000,
+               "max_length": 8000,
+               "debug": True
+               }
     )
+
 
     for item in output:
         response += item
